f334791c01bcaf8baef02269150f79a33c119428
P Campbell <pcampbell.edu@gmail.com>
2016-10-08 20:30:58 -0400

    script, some files added

--- modme.sh
+++ modme.sh
@@ -18,4 +18,17 @@ sleep 2
 touch datestamp-$(date +%F-%H:%M:%S)
 sleep 2
 touch datestamp-$(date +%F-%H:%M:%S)
- 
+
+if [ ! -d CS ] ; then 
+mkdir CS
+cd CS
+for i in bash c python c++ java
+  do
+  echo "great language! $(date +%F)" >  language-$i.TXT
+  done
+for i in  ghopper alovelace agoldberg mmeltzer bholberton
+  do
+  echo "pioneer in computer science" > team-$i.Txt
+  done
+fi
+
