37c457afe2561e92abefb4308bcb916bd7a606da
Maja Frydrychowicz <mjzffr@gmail.com>
2016-10-13 15:11:43 -0400

    Generate img files in modme.sh

--- modme.sh
+++ modme.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # PMC
 # October 2016
-# set up files w different content & time stamps 
-# for use in find exercises
+# set up files w different content & time stamps
+# for use in find exercises and other demos
 
 
 touch -d "36 hours ago" writing/*
@@ -19,7 +19,7 @@ touch datestamp-$(date +%F-%H:%M:%S)
 sleep 2
 touch datestamp-$(date +%F-%H:%M:%S)
 
-if [ ! -d CS ] ; then 
+if [ ! -d CS ] ; then
 mkdir CS
 cd CS
 for i in bash c python c++ java
@@ -32,3 +32,21 @@ for i in  ghopper alovelace agoldberg mmeltzer bholberton
   done
 fi
 
+# Make a bunch of fake image files with different timestamps
+cd -
+if [ ! -d images_example ] ; then
+  mkdir images_example
+  cd images_example
+  for i in {1532..1539}
+     do
+     touch -d 20120630 "IMG_$i.jpg"
+     done
+  for i in {1540..1552}
+     do
+     touch -d 20120701 "IMG_$i.jpg"
+     done
+  for i in {1553..1566}
+     do
+     touch -d 20120703 "IMG_$i.jpg"
+     done
+fi
